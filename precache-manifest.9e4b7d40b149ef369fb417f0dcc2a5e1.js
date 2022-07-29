9a7b9e86b06f53e93c09aaa52e844b0522d059a6
Teri11 <augustineteri157@gmail.com>
2022-07-30 00:27:43 +0300

    Updates

---
+++ precache-manifest.9e4b7d40b149ef369fb417f0dcc2a5e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b986c2362c8b4a95b91ee5b3a25758c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d596b92f068f5e05950d",
+    "url": "/static/css/2.6f4ee0a7.chunk.css"
+  },
+  {
+    "revision": "904a6d6e02a96b868d77",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "d596b92f068f5e05950d",
+    "url": "/static/js/2.3696376b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.3696376b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904a6d6e02a96b868d77",
+    "url": "/static/js/main.dd39614a.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
